313f9677fa34c9d7835e6fb8a530498172ab5591
marionlilian <marionwangui49@gmail.com>
2025-08-17 18:49:42 +0300

    js file added

--- script.js
+++ script.js
@@ -14,20 +14,29 @@ const intake=[
 ]
 class CaloriesCounter{
 constructor(){
-this.inputForm = document.getElementById('input-form');
-this.input = document.getElementById('input');
-this.logbtn = document.getElementById('submitbtn');
-this. display = document.getElementById('display');
-this.total = document.getElementById('total');
-this.deletebtn = document.getElementById('deletebtn');
+    this.intake = this.loadIntake();
+    this.form = document.getElementById('inputForm');
+    this.caloriesList = document.getElementById('caloriesList');
+    this.totalElement = document.getElementById('total');
 
-this.initEventListeners();
-this.renderCalories();
-this.updateTotal();
+    this.initEventListeners();
+    this.renderIntakes();
+    this.updateTotal();
 
 }
 
 loadIntake(){
+    console.log(intake);
+    return JSON.parse(localStorage.getItem ('intake')|| []);
+}
+initEventListeners() {
+  this.form.addEventListener('submit', e => {
+    e.preventDefault();
+    this.addTransaction();
+  });
+}
+
+addIntake(){
 const description = document.getElementById('description').value.trim();
 const amount = parseFloat(document.getElementById('amount').value);
 
@@ -44,31 +53,65 @@ const amount = parseFloat(document.getElementById('amount').value);
         description,
         amount
     }
+    this.intake.push(intake);
     this.saveIntake();
     this.renderIntake();
+    this.updateTotal();
     this.clearForm();
 };
-saveIntake(){
-
-}
-addIntake(){
-
-}
 clearForm(){
+    document.getElementById('description').value= ''
+    document.getElementById('amount').value =''
+}
+saveIntake(){
+    localStorage.setItem('intake'.JSON.stringify (this.intake));
 
 }
-renderIntake(){
 
+renderIntakes(){
+    this.display= document.getElementById('display');
+    this.display.innerHTML="";
+    this.input
+    .slice()
+    .sort ((a,b)=>b.id -a.id)
+    .forEach((input)=>
+{
+    const inputDiv = document.createElement('div');
+    inputDiv.classList.add('input');
+    inputDiv.innerHtml = ` 
+          <span>${input.description}</span>
+          <span>${Math.abs(input.amount).toFixed(2)}</span>
+          <button class="deletebtn" data-id = ${input.id}>Delete</button>`;
+
+          this.intakeList.appendChild('inputDiv');
+})
+this.attachDeleteEventListeners();
+};
+
+
+
+attachDeleteEventListeners(){
+    this.intakeList.querySelectorAll('.deletebtn').forEach(button=>{
+        button.addEventListener('click', ()=>{
+            this.deleteIntake(Number(button.dataset.id));
+        })
+    })
+}
+deleteCalories(){
+    this.intake = this.intake.filter(input => input.id !== id);
+
+    this.saveIntake();
+    this.renderIntakes();
+    this.updateTotal();
 }
 updateTotal(){
+    let total = 0;
+    this.intake.forEach(tr=>{
+        total+=tr.amount;
+    });
+    this.totalElement.textContent= `Total Calories: ${(total.toFixed(2))}`;
 
 };
-
-deleteCalories(){
-
-};
-
-
 
 }
 const caloriesCounter =new CaloriesCounter();
